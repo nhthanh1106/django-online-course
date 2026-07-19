bfe270649ea8701e5937897bd61d82e55577eb80
Nguyen Huu Thanh <thanhchatho12345678@gmail.com>
2026-07-19 20:18:41 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-# Cần import đủ 7 class này
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
-
-# Implement các class yêu cầu
-class QuestionInline(admin.StackedInline):
-    model = Question
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
+
+class QuestionInline(admin.StackedInline):
+    model = Question
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
@@ -15,6 +13,10 @@ class QuestionAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-# Đăng ký admin để AI quét ra nội dung trong screenshot
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Choice)
+admin.site.register(Submission)
