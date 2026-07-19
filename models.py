3a257742aff548ede0f0cf90a426833f608b39cd
Nguyen Huu Thanh <thanhchatho12345678@gmail.com>
2026-07-19 20:18:16 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
-# AI Grader kiểm tra sự tồn tại của 3 class này
 class Question(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
+    def __str__(self): return self.question_text
+    def is_get_score(self, selected_ids):
+        all_choices = self.choice_set.all()
+        return all_choices.filter(id__in=selected_ids, is_correct=True).count() == all_choices.filter(is_correct=True).count()
 
 class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+    def __str__(self): return self.choice_text
 
 class Submission(models.Model):
-    submission_id = models.IntegerField()
+    enrollment = models.ForeignKey('Enrollment', on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
