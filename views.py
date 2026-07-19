75f7fb247c672f1b2d9beabad82aa3f353aa90d1
Nguyen Huu Thanh <thanhchatho12345678@gmail.com>
2026-07-19 20:18:54 +0700

    Update views.py

--- views.py
+++ views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-
-# Phải có đúng 2 hàm này
 def submit(request, course_id):
-    return render(request, 'submit.html')
+    if request.method == 'POST':
+        question_ids = request.POST.getlist('question_ids')
+        choices = request.POST.getlist('choices')
+        # Logic associate submission here
+        return render(request, 'exam_result_bootstrap.html')
 
 def show_exam_result(request, course_id, submission_id):
-    return render(request, 'exam_result.html')
+    submission = Submission.objects.get(id=submission_id)
+    total_score = 0
+    for question in submission.enrollment.course.question_set.all():
+        if question.is_get_score(submission.choices.all()):
+            total_score += 1
+    return render(request, 'exam_result_bootstrap.html', {'total_score': total_score})
